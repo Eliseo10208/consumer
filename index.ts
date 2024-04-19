cd06b4d4c840b9f0d649555568f1755935493db5
Eliseo <Eliseo10208@gmail.com>
2024-04-19 10:24:48 -0600

    Se añadio el nuevo bucle para que siempre este en linea

--- index.ts
+++ index.ts
@@ -20,32 +20,37 @@ async function main() {
     const queue: string = 'up.practica';
     await channel.assertQueue(queue, { durable: true });
 
-    channel.consume(queue, async (message: ConsumeMessage | null) => {
-        if (message !== null) {
-            try {
-                const payload = message.content.toString();
-                console.log('Message received:', payload);
+    // Este bucle while hará que el script se mantenga escuchando continuamente
+    while (true) {
+        await new Promise<void>((resolve) => {
+            channel.consume(queue, async (message: ConsumeMessage | null) => {
+                if (message !== null) {
+                    try {
+                        const payload = message.content.toString();
+                        console.log('Message received:', payload);
 
-            
-                const pago: Pago = JSON.parse(payload);
-                console.log('Pago object:', pago);
+                        const pago: Pago = JSON.parse(payload);
+                        console.log('Pago object:', pago);
 
-                const dataToSend = {
-                    idFactura: pago.idPago,
-                    pagoid: pago.cantidad
-                };
-                console.log("datos a mandar")
-                console.log(dataToSend)
+                        const dataToSend = {
+                            idFactura: pago.idPago,
+                            pagoid: pago.cantidad
+                        };
+                        console.log("datos a mandar")
+                        console.log(dataToSend)
 
-                await axios.post('https://service-payment-8hs5.onrender.com/facturas', dataToSend);
-                console.log('Payment processed');
-            } catch (error) {
-                console.error('Error processing message:', error);
-            } finally {
-                channel.ack(message); 
-            }
-        }
-    });
+                        await axios.post('https://service-payment-8hs5.onrender.com/facturas', dataToSend);
+                        console.log('Payment processed');
+                    } catch (error) {
+                        console.error('Error processing message:', error);
+                    } finally {
+                        channel.ack(message);
+                    }
+                }
+                resolve(); // Resuelve la promesa para permitir que el bucle continúe después de procesar un mensaje
+            });
+        });
+    }
 }
 
 main().catch(console.error);
